19936e011a6925eebd534c2ee3b8db827ddbb82f
tskoyo <jovansr@protonmail.com>
2025-03-16 10:29:17 +0100

    Add custom validation error

--- src/tests/createPage.test.ts
+++ src/tests/createPage.test.ts
@@ -1,5 +1,9 @@
 import { it, expect, describe } from 'vitest';
-import { buildPayload, createNotionPage } from '../actions/pages/createPage.ts';
+import {
+  buildPayload,
+  createNotionPage,
+  CreateNotionPageParams,
+} from '../actions/pages/createPage.ts';
 import 'dotenv/config';
 
 describe('Create page', () => {
@@ -9,18 +13,30 @@ describe('Create page', () => {
   describe('invalid page id', () => {
     it('should not create page', async () => {
       const pageId = '123';
-      const payload = buildPayload(pageId, title);
 
+      const payload = { id: pageId, title: title } as CreateNotionPageParams;
       const page = await createNotionPage(apiKey, payload);
 
       expect(page).toBe(null);
     }, 5000);
   });
 
+  describe('empty title', () => {
+    it('should not create page', async () => {
+      const pageId = '1aabd338-ff88-80ec-92ea-ce2e6857ba6c';
+      const title = '';
+
+      const payload = { id: pageId, title: title } as CreateNotionPageParams;
+      const page = await createNotionPage(apiKey, payload);
+
+      expect(page).toBe(null);
+    });
+  });
+
   describe('valid page id', () => {
     it('should create a new page', async () => {
       const pageId = '1aabd338-ff88-80ec-92ea-ce2e6857ba6c';
-      const payload = buildPayload(pageId, title);
+      const payload = { id: pageId, title: title } as CreateNotionPageParams;
 
       const page = await createNotionPage(apiKey, payload);
 
